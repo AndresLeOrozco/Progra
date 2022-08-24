67f21502928fcd88069cc29849344b63ef8bc6eb
Andres <andres.leon.orozco@est.una.ac.cr>
2022-08-23 19:08:14 -0600

    NoFinalizado

---
+++ Cifrado.java
@@ -0,0 +1,54 @@
+
+public class Cifrado {
+private final char [] abecedario;
+private final char [] abecedarioMayus;
+
+Cifrado(){
+
+    abecedario = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    abecedarioMayus = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+   //crear arreglo
+    //tabla ASCII
+}
+ public String CifrarPalabra(int espacios,String palabra){
+    String cif = "";
+    char[] vec = new char[20];
+    int k = 0;
+    int LargoPalabra = palabra.length();
+    while(k < LargoPalabra) {
+        vec[k] = cif.charAt(k);
+        k++;
+    }
+    int can = 0;
+    while(can < LargoPalabra) {
+        for(int i=0;i<26;i++) {
+            if (vec[can] == abecedario[i]){
+                if(i + espacios > 25){
+                    int cNueva = (i+espacios)-25;
+                    vec[can] = abecedario[cNueva];
+                }
+                if(i + espacios < 25)
+                    vec[can] = abecedario[i+espacios];
+            }
+
+            if (vec[can] == abecedarioMayus[i]){
+                if(i + espacios > 25){
+                    int cNueva = (i+espacios)-25;
+                    vec[can] = abecedarioMayus[cNueva];
+                }
+                if(i + espacios < 25)
+                    vec[can] = abecedarioMayus[i+espacios];
+            }
+        }
+        can++;
+    }
+    for(int i=0;i<can;i++) {
+        cif += String.valueOf(vec[i]);
+    }
+    return cif;
+    }
+
+    }
+
+
+
---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Cifrado palabra = new Cifrado();
+
+        System.out.println(palabra.CifrarPalabra(2,"Pedro"));
+    }
+}
