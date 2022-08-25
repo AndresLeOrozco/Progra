574e959b8f5c06264c552002bd0e937a16d45fc3
andre <andres.leon.orozco@est.una.ac.cr>
2022-08-25 09:54:41 -0600

    Ya funciona el  cifrado y descifrado

--- Cifrado.java
+++ Cifrado.java
@@ -1,62 +1,99 @@
 
 public class Cifrado {
-private final char [] abecedario;
-private final char [] abecedarioMayus;
+    private final char[] abecedario;
+    private final char[] abecedarioMayus;
 
-Cifrado(){
+    Cifrado() {
 
-    abecedario = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    abecedarioMayus = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-   //crear arreglo
-    //tabla ASCII
-}
- public String CifrarPalabra(int espacios,String palabra){
-    String cif = "";
-    char[] vec = new char[palabra.length()];
-    int k = 0;
-    int LargoPalabra = palabra.length();
-    while(k < LargoPalabra) {
-        vec[k] = palabra.charAt(k);
-        k++;
+        abecedario = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        abecedarioMayus = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        //crear arreglo
+        //tabla ASCII
     }
-    int can = 0;
-    while(can < LargoPalabra) {
-        for(int i=0;i<26;i++) {
-            if (vec[can] == abecedario[i]){
-                if(i + espacios > 25){
-                    int cNueva = (i+espacios)-25;
-                    vec[can] = abecedario[cNueva];
-                    break;
-                }
-                else
-                if(i + espacios < 25) {
-                    vec[can] = abecedario[i + espacios];
-                    break;
-                }
-            }
 
-            if (vec[can] == abecedarioMayus[i]){
-                if(i + espacios > 25){
-                    int cNueva = (i+espacios)-25;
-                    vec[can] = abecedarioMayus[cNueva];
-                    break;
-                }
-                else
-                if(i + espacios < 25) {
-                    vec[can] = abecedarioMayus[i + espacios];
-                    break;
-                }
-            }
+    public String CifrarPalabra(int espacios, String palabra) {
+        String cif = "";
+        char[] vec = new char[palabra.length()];
+        int k = 0;
+        int LargoPalabra = palabra.length();
+        while (k < LargoPalabra) {
+            vec[k] = palabra.charAt(k);
+            k++;
         }
-        can++;
-    }
-    for(int i=0;i<can;i++) {
-        cif += String.valueOf(vec[i]);
-    }
-    return cif;
+        int can = 0;
+        while (can < LargoPalabra) {
+            for (int i = 0; i < 26; i++) {
+                if (vec[can] == abecedario[i]) {
+                    if (i + espacios > 25) {
+                        int cNueva = (i + espacios) - 25;
+                        vec[can] = abecedario[cNueva - 1];
+                        break;
+                    } else if (i + espacios < 25) {
+                        vec[can] = abecedario[i + espacios];
+                        break;
+                    }
+                }
+
+                if (vec[can] == abecedarioMayus[i]) {
+                    if (i + espacios > 25) {
+                        int cNueva = (i + espacios) - 25;
+                        vec[can] = abecedarioMayus[cNueva - 1];
+                        break;
+                    } else if (i + espacios < 25) {
+                        vec[can] = abecedarioMayus[i + espacios];
+                        break;
+                    }
+                }
+            }
+            can++;
+        }
+        for (int i = 0; i < can; i++) {
+            cif += String.valueOf(vec[i]);
+        }
+        return cif;
     }
 
-    }
+    public String Descifrado(String palabra, int espacios) {
+        String cif = "";
+        char[] vec = new char[palabra.length()];
+        int k = 0;
+        int LargoPalabra = palabra.length();
+        while (k < LargoPalabra) {
+            vec[k] = palabra.charAt(k);
+            k++;
+        }
+        int can = 0;
+        while (can < LargoPalabra) {
+            for (int i = 0; i < 26; i++) {
+                if (vec[can] == abecedario[i]) {
+                    if (i - espacios < 0) {
+                        int cNueva = 26 + (i - espacios);
+                        vec[can] = abecedario[cNueva];
+                        break;
+                    } else if (i - espacios >= 0) {
+                        vec[can] = abecedario[i - espacios];
+                        break;
+                    }
+                }
 
+                if (vec[can] == abecedarioMayus[i]) {
+                    if (i - espacios < 0) {
+                        int cNueva = 26 + (i - espacios);
+                        vec[can] = abecedarioMayus[cNueva];
+                        break;
+                    } else if (i - espacios >= 0) {
+                        vec[can] = abecedarioMayus[i - espacios];
+                        break;
+                    }
+                }
+            }
+            can++;
+        }
+        for (int i = 0; i < can; i++) {
+            cif += String.valueOf(vec[i]);
+        }
+        return cif;
+    }
+}
 
 
--- main.java
+++ main.java
@@ -1,7 +1,9 @@
 public class main {
     public static void main(String[] args) {
         Cifrado palabrinha = new Cifrado();
+       String palCifrada = palabrinha.CifrarPalabra(4,"Z1bra");
+       System.out.println(palCifrada);
+       System.out.println(palabrinha.Descifrado(palCifrada,4));
 
-        System.out.println(palabrinha.CifrarPalabra(2,"Pedro"));
     }
 }
